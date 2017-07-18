0decfbfac2f6ac721c8aba30ad845aa1090986e4
Alvaro Denis <denisacostaq@gmail.com>
2017-07-18 11:00:26 -0400

    ref #4 seriliaztion and db queries on Item and Product.

--- app/models/Item.java
+++ app/models/Item.java
@@ -1,34 +1,156 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.Min;
-
+import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.data.validation.Constraints.Min;
 import play.db.ebean.Model;
 
 @Entity(name = "t_items")
 public class Item extends Model {
 
+    private interface DefaultView {
+    }
+
+    //          ATTRIBUTES
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonView({DefaultView.class})
     private Long id;
 
+    @JsonView({DefaultView.class})
     private String name;
 
     @Min(0)
+    @JsonView({DefaultView.class})
     private Long stock;
 
     @ManyToOne
     private Product product;
 
+    //          DB OPERATIONS
+    private static final Finder<Long, Item> finder = new Finder<>(Long.class, Item.class);
+
+    /**
+     * This method search in the database all the Items.
+     *
+     * @return List<Item>
+     * @throws Exception
+     */
+    public static List<Item> getList() throws Exception {
+        return finder.all();
+    }
+
+    /**
+     * This method create an Item in database.
+     *
+     * @param item
+     * @throws Exception
+     */
+    public static void create(Item item) throws Exception {
+        item.save();
+    }
+
+    /**
+     * This method search in the database looking for an unique Item using the key and the corresponding
+     * object given by the parameters.
+     *
+     * @param key
+     * @param obj
+     * @return Item
+     * @throws java.lang.Exception
+     */
+    public static Item findByPropertie(String key, Object obj) throws Exception {
+        return finder.where().eq(key, obj).findUnique();
+    }
+
+    /**
+     * This method search in the database looking for all Items using the key and the corresponding object
+     * given by the parameters.
+     *
+     * @param key
+     * @param obj
+     * @return List<Item>
+     * @throws java.lang.Exception
+     */
+    public static List<Item> findAllByPropertie(String key, Object obj) throws Exception {
+        return finder.where().eq(key, obj).findList();
+    }
+
+    /**
+     * This method update an Item.
+     *
+     * @param item
+     * @throws Exception
+     */
+    public static void update(Item item) throws Exception {
+        item.update();
+    }
+
+    /**
+     * This method delete an Item
+     *
+     * @param item
+     * @throws Exception
+     */
+    public static void delete(Item item) throws Exception {
+        item.delete();
+    }
+
+    //          SERIALIZATION
+    /**
+     * Serialize an eItem with DefaultView annotated members.
+     *
+     * @return JsonNode
+     * @throws Exception
+     */
+    public JsonNode jsonSerialization() throws Exception {
+        return jsonSerialization(DefaultView.class);
+    }
+
+    /**
+     * Serialize an Item with a particular set of attributes.
+     *
+     * @param view
+     * @return JsonNode
+     * @throws IOException
+     */
+    public JsonNode jsonSerialization(Class view) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
+        ObjectWriter writer = mapper.writerWithView(view);
+        JsonNode response = mapper.readTree(writer.writeValueAsString(this));
+        return response;
+    }
+
+    /**
+     * Serialize a list of ItemStates with all the annotated class members.
+     *
+     * @param items
+     * @return
+     * @throws Exception
+     */
+    public static JsonNode jsonListSerialization(List<Item> items) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        List<ObjectNode> list = new ArrayList();
+        for (Item item : items) {
+            list.add(mapper.convertValue(item.jsonSerialization(), ObjectNode.class));
+        }
+        return mapper.convertValue(list, JsonNode.class);
+    }
+
+    //          GET/SET
     public Long getId() {
         return id;
     }
--- app/models/Product.java
+++ app/models/Product.java
@@ -1,29 +1,139 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
-import javax.persistence.Column;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.data.validation.Constraints.MaxLength;
 import play.db.ebean.Model;
 
 @Entity(name = "t_products")
 public class Product extends Model {
 
+    private interface DefaultView {
+    }
+
+    //          ATTRIBUTES
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonView({DefaultView.class})
     private Long id;
 
+    @JsonView({DefaultView.class})
     private String name;
 
-    @Column(length = 500)
+    @MaxLength(message = "500 caracteres como maximo para la descripcion", value = 500)
+    @JsonView({DefaultView.class})
     private String description;
 
+    //          DB OPERATIONS
+    private static final Finder<Long, Product> finder = new Finder<>(Long.class, Product.class);
+
+    /**
+     * This method search in the database all the Products.
+     *
+     * @return List<Product>
+     * @throws Exception
+     */
+    public static List<Product> getList() throws Exception {
+        return finder.all();
+    }
+
+    /**
+     * This method create an Product in database.
+     *
+     * @param product
+     * @throws Exception
+     */
+    public static void create(Product product) throws Exception {
+        product.save();
+    }
+
+    /**
+     * This method search in the database looking for an unique Product using the key and the corresponding
+     * object given by the parameters.
+     *
+     * @param key
+     * @param obj
+     * @return Product
+     * @throws java.lang.Exception
+     */
+    public static Product findByPropertie(String key, Object obj) throws Exception {
+        return finder.where().eq(key, obj).findUnique();
+    }
+
+    /**
+     * This method update an Product.
+     *
+     * @param product
+     * @throws Exception
+     */
+    public static void update(Product product) throws Exception {
+        product.update();
+    }
+
+    /**
+     * This method delete an Product
+     *
+     * @param product
+     * @throws Exception
+     */
+    public static void delete(Product product) throws Exception {
+        product.delete();
+    }
+
+    //          SERIALIZATION
+    /**
+     * Serialize an eProduct with DefaultView annotated members.
+     *
+     * @return JsonNode
+     * @throws Exception
+     */
+    public JsonNode jsonSerialization() throws Exception {
+        return jsonSerialization(DefaultView.class);
+    }
+
+    /**
+     * Serialize an Product with a particular set of attributes.
+     *
+     * @param view
+     * @return JsonNode
+     * @throws IOException
+     */
+    public JsonNode jsonSerialization(Class view) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
+        ObjectWriter writer = mapper.writerWithView(view);
+        JsonNode response = mapper.readTree(writer.writeValueAsString(this));
+        return response;
+    }
+
+    /**
+     * Serialize a list of ProductStates with all the annotated class members.
+     *
+     * @param products
+     * @return
+     * @throws Exception
+     */
+    public static JsonNode jsonListSerialization(List<Product> products) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        List<ObjectNode> list = new ArrayList();
+        for (Product product : products) {
+            list.add(mapper.convertValue(product.jsonSerialization(), ObjectNode.class));
+        }
+        return mapper.convertValue(list, JsonNode.class);
+    }
+
+    //          GET/SET
     public Long getId() {
         return id;
     }
